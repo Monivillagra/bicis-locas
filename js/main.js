e66a98198833725fbb14ae99ef3c5f1187e5e0a3
Monivillagra <villagra.ramirez@gmail.com>
2016-10-25 23:41:14 -0300

    se agrega validaciones de contraseñas, el tipo de bici

--- js/main.js
+++ js/main.js
@@ -1,37 +1,44 @@
 function validateForm(){
-	/* Escribe tú código aquí */
 	//Todos los campos son obligatorios, excepto los dos últimos.
-//Los campos nombre y apellido sólo deben permitir caracteres de la A-Z   OK
-//Para los campos nombre y apellido la primera letra debe ser mayúscula   OK
-//Validar que el campo email tenga un formato válido. Ej: name@domain.com OK
-//El campo password debe tener al menos 6 caracteres                      OK
-//El campo password no puede ser igual a "password" ó "123456" ó "098754"
-//El valor seleccionado de bicis, debe ser una de las opciones presentadas
-//Si algún campo presenta error debe informarse al usuario por medio de un alert.
-    var name=document.getElementById('name').value;
-    var lastName=document.getElementById('lastname').value;
-    var email=document.getElementById('input-email').value;
-    var password=document.getElementById('input-password').value;
+    var name = document.getElementById('name').value;
+    var lastName = document.getElementById('lastname').value;
+    var email = document.getElementById('input-email').value;
+    var password = document.getElementById('input-password').value;
+    var bicis = document.querySelector("select").value;
 //name
+//Los campos nombre y apellido sólo deben permitir caracteres de la A-Z   OK
+//Para los campos nombre y apellido la primera letra debe ser mayúscula
     if( name == null || name.length <= 1 || /^\s+$/.test(name) || /^[A-Za-z]*$/.test(name) == false || name.substring(0,1) == name.substring(0,1).toUpperCase() == false ) {
-        alert("Ingrese Su Nombre Correctamente"); 
+        alert("Escriba el primer caracter de su nombre en Mayúscula."); 
         return false;    
 	}
 //lastname
 	if( lastName == null || lastName.length == 0 || /^\s+$/.test(lastName) || /^[A-Za-z]*$/.test(lastName) == false || lastName.substring(0,1) == lastName.substring(0,1).toUpperCase() == false ) { 
-        alert("Ingrese Su Apellido Correctamente"); 
+        alert("Escriba el primer caracter de su apellido en Mayúscula."); 
         return false;   
 	}
 //email
+//Validar que el campo email tenga un formato válido. Ej: name@domain.com
     if ( email == null || email.length == 0 || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email) != true ) {
-        alert("Dirección de Correo Electrónico No Válido");
-        return false;  
+        alert("Dirección de Correo Electrónico No Válido!");
+        return false;   
    } 
 //password
-	if( password == null || password.length < 6 || /^\s+$/.test(password) ) {
-        alert("Ingrese una contraseña que contenga un mínimo de 6 caracteres!");    
+//El campo password debe tener al menos 6 caracteres                      OK
+//El campo password no puede ser igual a "password" ó "123456" ó "098754"
+     if ( password == null || password.length < 6 ) {
+        alert ("Contraseña No Valida, debe tener como mínimo 6 Caracteres!")
         return false;
-	}
-
+    } else if ( password == '123456' || password == '098754' || password == 'password' ) {
+        alert ("Su Contraseña No es Segura!");
+        return false;
+    }
+//campo obligatorio
+//El valor seleccionado de bicis, debe ser una de las opciones presentadas
+//Si algún campo presenta error debe informarse al usuario por medio de un alert.
+    if (bicis == 0) {
+        alert("Seleccione su Tipo de Bici!");
+        return false;
+    }
 }
 //validateForm();
